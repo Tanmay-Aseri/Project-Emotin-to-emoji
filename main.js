8cebd259fdcad1c812fbc9de92c616f7c2aec986
Tanmay Aseri <87698651+Tanmay-Aseri@users.noreply.github.com>
2023-03-28 16:30:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //https://teachablemachine.withgoogle.com/models/hvBA5dxBh/
-ebcam.set({
+Webcam.set({
     width: 350,
     height: 300,
     image_format : 'png',
@@ -14,7 +14,7 @@ function take_snapshot(){
       Webcam.snap(function(data_uri) {
         document.getElementById("result").innerHTML = '<img id = "captured_image" src= " '+data_uri+'"/>';
 
-    }
+    })
 }
 
 
@@ -23,4 +23,41 @@ classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models
 
 function modelLoaded(){
     console.log("Model Loaded!");
-};
+}
+
+function check(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+if(error) {
+    console.log(error);
+} else {
+    console.log(results);
+    document.getElementById("result-emotion_name").innerHTML = results[0].label ; 
+    prediction_1 = results[0].label;
+    speak();
+
+    if(prediction_1 == "Thumbs up"){
+        document.getElementById("update_emoji").innerHTML = "&#128077;";
+    }
+    if(prediction_1 == "amazing"){
+        document.getElementById("update_emoji").innerHTML = "&#128076;";
+    }
+    if(prediction_1 == "Victory"){
+        document.getElementById("update_emoji").innerHTML = "&#9996;";
+    }
+
+
+}
+}
+
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is"+prediction_1;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1);
+    synth.speak(utterThis);
+}
